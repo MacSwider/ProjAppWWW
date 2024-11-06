bbd072b53a285550a8d51f8ab9ed03b25a3358cc
MacSwider <maciejswiderbusiness@gmail.com>
2024-11-06 19:39:10 +0100

    .

--- lab5/strona_v1.4/index.php
+++ lab5/strona_v1.4/index.php
@@ -13,49 +13,40 @@
 
     <nav>
         <ul>
-            <<li><a href="index.php">Strona Główna</a></li>
-            <li><a href="index.php?page=rzesza">III Rzesza w Kosmosie</a></li>
-            <li><a href="index.php?page=pierwsze-misje">Pierwsze Misje Kosmiczne</a></li>
-            <li><a href="index.php?page=misje-zalogowe">Misje Załogowe</a></li>
-            <li><a href="index.php?page=stacje">Stacje Kosmiczne</a></li>
-            <li><a href="index.php?page=komercjalizacja">Komercjalizacja Kosmosu</a></li>
-            <li><a href="index.php?page=kontakt">Kontakt</a></li>
-            <li><a href="index.php?page=skrypty">Skrypty (Lab2)</a></li>
-            <li><a href="index.php?page=animacje">Animacje (Lab3)</a></li>
+            <li><a href="index.php">Strona Główna</a></li>
+            <li><a href="html/rzesza.html">III Rzesza w Kosmosie</a></li>
+            <li><a href="html/pierwsze-misje.html">Pierwsze Misje Kosmiczne</a></li>
+            <li><a href="html/misje-zalogowe.html">Misje Załogowe</a></li>
+            <li><a href="html/stacje.html">Stacje Kosmiczne</a></li>
+            <li><a href="html/komercjalizacja.html">Komercjalizacja Kosmosu</a></li>
+            <li><a href="html/kontakt.html">Kontakt</a></li>
+            <li><a href="html/skrypty.html">Skrypty (Lab2)</a></li>
+            <li><a href="html/animacje.html">Animacje (Lab3)</a></li>
         </ul>
     </nav>
-
-    
 
     <div class="container">
         <h2>Witamy na stronie o eksploracji kosmosu!</h2>
         <p class="text-content">
-            
-       
-    <p>
-        Poniższa strona została stworzona w ramach zajeć z Programowania Aplikacji WWW i porusza temat historii lotów kosmicznych. 
-
+        Poniższa strona została stworzona w ramach zajęć z Programowania Aplikacji WWW i porusza temat historii lotów kosmicznych. 
         Wykonane z użyciem HTML i CSS
-
+        </p>
         
-    </p>
-    
-     
-    <p>
-    Przyciski testowe. Brak zaimplementowanej funkcji
-    <button class="button" disabled>Przycisk 1</button>
-    <button class="button" disabled>Przycisk 2</button>  
-    </p>    
-
+        <p>
+        Przyciski testowe. Brak zaimplementowanej funkcji
+        <button class="button" disabled>Przycisk 1</button>
+        <button class="button" disabled>Przycisk 2</button>  
+        </p>    
     </div> 
 
     <footer>
         &copy; 2024 Historia Kosmosu
     </footer>
+
     <?php
-        $nr_indeksu = '169370';
-        $nrGrupy = '4';
-        echo 'Autor: Maciej Świder '.$nr_indeksu.' grupa '.$nrGrupy.' <br /><br />';
+    $nr_indeksu = '169370';
+    $nrGrupy = '4';
+    echo 'Autor: Maciej Świder ' . $nr_indeksu . ' grupa ' . $nrGrupy . ' <br /><br />';
     ?>
 </body>
 </html>
