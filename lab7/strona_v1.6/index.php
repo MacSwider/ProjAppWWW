d30bd6fea16335e9d1a54a1d541d361a669bab74
MacSwider <maciejswiderbusiness@gmail.com>
2024-11-27 20:29:35 +0100

    Update index.php

--- lab7/strona_v1.6/index.php
+++ lab7/strona_v1.6/index.php
@@ -15,11 +15,11 @@
 
     $Admin = new Admin();
 
-    // Wymuszanie logowania na samym początku
+   
     if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
         echo $Admin->FormularzLogowania();
         session_destroy();
-        exit; // Przerywa dalsze ładowanie strony
+        exit; 
     }
 ?>
 
@@ -27,16 +27,16 @@
         <nav id="navbar">
             <ul>
                 <li><a href="index.php?idp=1">Strona główna</a></li>
-                <li><a href="index.php?idp=2">Animacje</a></li>
-                <li><a href="index.php?idp=3">Komercjalizacja</a></li>
-                <li><a href="index.php?idp=4">Kontakt</a></li>
-                <li><a href="index.php?idp=5">Misje zalogowe</a></li>
-                <li><a href="index.php?idp=6">Pierwsze misje</a></li>
-                <li><a href="index.php?idp=7">Rzesza</a></li>
-                <li><a href="index.php?idp=8">Skrypty</a></li>
-                <li><a href="index.php?idp=9">Stacje</a></li>
-                <li><a href="index.php?idp=10">Filmy</a></li>
-                <li><a href="index.php?idp=-1">Lista Podstron</a></li> <!-- Dodany element nawigacyjny -->
+                <li><a href="index.php?idp=2">Rzesza</a></li>
+                <li><a href="index.php?idp=3">Pierwsze misje</a></li>
+                <li><a href="index.php?idp=4">Misje zalogowe</a></li>
+                <li><a href="index.php?idp=5">Stacje</a></li>
+                <li><a href="index.php?idp=6">Komercjalizacja</a></li>
+                <li><a href="index.php?idp=7">Kontakt</a></li>
+                <li><a href="index.php?idp=8">Animacje</a></li>
+                <li><a href="index.php?idp=9">Filmy</a></li>
+                <li><a href="index.php?idp=10">Skrypty</a></li>
+                <li><a href="index.php?idp=-1">Lista Podstron</a></li> 
             </ul>
         </nav>
     </header>
@@ -45,23 +45,23 @@
 <?php
     include('showpage.php');
 
-    // Obsługa różnych działań na podstawie parametru `idp`.
+   
     $id = htmlspecialchars($_GET['idp'] ?? '1');
 
     switch ($id) {
-        case '-1': // Lista podstron
+        case '-1': 
             $Admin->ListaPodstron();
             break;
-        case '-2': // Edycja strony
+        case '-2': 
             echo $Admin->EditPage();
             break;
-        case '-3': // Usunięcie strony
+        case '-3': 
             echo $Admin->DeletePage();
             break;
-        case '-4': // Tworzenie nowej strony
+        case '-4': 
             echo $Admin->CreatePage();
             break;
-        default: // Wyświetlenie zawartości strony
+        default: 
             echo PokazStrone($id);
             break;
     }
@@ -70,7 +70,7 @@
 
 <footer>
     <?php
-    // Informacje o autorze
+    
     $nr_indeksu = '169370';
     $nrGrupy = '4';
     echo 'Autor: Maciej Świder ' . $nr_indeksu . ' grupa ' . $nrGrupy . '<br/><br/>';
